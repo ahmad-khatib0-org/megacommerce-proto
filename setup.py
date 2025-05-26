fa104743c9ba22b59118b24b99677ea8d75af7ab
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-05-26 19:21:40 +0300

    pyton setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="megacommerce_proto",
+    version="0.1.0",
+    packages=find_packages(where="gen/python"),
+    package_dir={"": "gen/python"},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "grpcio",
+        "protobuf",
+    ],
+)
