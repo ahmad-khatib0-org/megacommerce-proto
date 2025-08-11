95bb28b7b106c5ca6d3efaf3e5597f8e59433110
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-08-11 18:24:37 +0300

    generate *_pb.js files for grpc-web

---
+++ wrappers/proto-npm/src/web/common/v1/common_pb.js
@@ -0,0 +1,29 @@
+// source: common/v1/common.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global =
+    (typeof globalThis !== 'undefined' && globalThis) ||
+    (typeof window !== 'undefined' && window) ||
+    (typeof global !== 'undefined' && global) ||
+    (typeof self !== 'undefined' && self) ||
+    (function () { return this; }).call(null) ||
+    Function('return this')();
+
+var common_v1_config_pb = require('../../common/v1/config_pb.js');
+goog.object.extend(proto, common_v1_config_pb);
+var common_v1_trans_pb = require('../../common/v1/trans_pb.js');
+goog.object.extend(proto, common_v1_trans_pb);
+var common_v1_types_pb = require('../../common/v1/types_pb.js');
+goog.object.extend(proto, common_v1_types_pb);
---
+++ wrappers/proto-npm/src/web/products/v1/products_pb.js
@@ -0,0 +1,29 @@
+// source: products/v1/products.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global =
+    (typeof globalThis !== 'undefined' && globalThis) ||
+    (typeof window !== 'undefined' && window) ||
+    (typeof global !== 'undefined' && global) ||
+    (typeof self !== 'undefined' && self) ||
+    (function () { return this; }).call(null) ||
+    Function('return this')();
+
+var products_v1_product_create_pb = require('../../products/v1/product_create_pb.js');
+goog.object.extend(proto, products_v1_product_create_pb);
+var products_v1_product_data_pb = require('../../products/v1/product_data_pb.js');
+goog.object.extend(proto, products_v1_product_data_pb);
+var products_v1_product_list_pb = require('../../products/v1/product_list_pb.js');
+goog.object.extend(proto, products_v1_product_list_pb);
