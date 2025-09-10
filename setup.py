d0f6ec4c59e1f8f4c780918c8ea5d94a6da3cca3
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-09-11 00:21:06 +0300

    increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="megacommerce_proto",
-    version="0.1.71",
+    version="0.1.73",
     packages=find_packages(where="gen/python"),
     package_dir={"": "gen/python"},
     include_package_data=True,
